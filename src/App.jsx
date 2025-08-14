1fdb32e521bb614ca5ac8073550ae6da5f71d44b
JIRafid <jubayerishfaqrafid@gmail.com>
2025-08-14 16:30:03 +0600

    Footer done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import { Button } from "@/components/ui/button"
+import Navbar from "./Components/Layout/Navbar";
+import Footer from "./Components/layout/Footer";
 
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-svh">
-      <Button>Click me</Button>
+    <div className="">
+
+      <Footer />
     </div>
-  )
+  );
 }
 
 export default App;
