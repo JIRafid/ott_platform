232832bd760953dfca9f89c0c8f18e2d4ee67ee7
tariqulp <therickypsm@gmail.com>
2025-08-14 16:35:18 +0600

    NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
-import { Button } from "@/components/ui/button"
+import { Routes, Route } from "react-router-dom";
+import TopNavbar from "./Components/layout/NavBar/TopNavbar";
+import BottomNavBar from "./Components/layout/NavBar/BottomNavBar";
+
+/* // Pages
+import Home from "./Pages/Home/Home";
+import Movies from "./Pages/Movies/Movies";
+import Series from "./Pages/Series/Series";
+import Shorts from "./Pages/Shorts/Shorts";
+import LiveTV from "./Pages/LiveTV/LiveTV";
+import Account from "./Pages/Account/Account"; */
 
 function App() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
+    <div className="background min-h-screen flex flex-col">
+      <TopNavbar />
+
+{/*       <div className="flex-1">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/series" element={<Series />} />
+          <Route path="/shorts" element={<Shorts />} />
+          <Route path="/live-tv" element={<LiveTV />} />
+          <Route path="/account" element={<Account />} />
+        </Routes>
+      </div> */}
+
+      <BottomNavBar />
     </div>
-  )
+  );
 }
 
 export default App;
