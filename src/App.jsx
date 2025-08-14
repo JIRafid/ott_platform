262d6ced6596e4a5a7ff7a902e9df37d98e6a1a6
JIRafid <jubayerishfaqrafid@gmail.com>
2025-08-14 11:27:28 +0600

    Installed all dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import { useState } from 'react'
-
-import './App.css'
+import { Button } from "@/components/ui/button"
 
 function App() {
-
   return (
-    <div></div>
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
   )
 }
 
-export default App
+export default App;
