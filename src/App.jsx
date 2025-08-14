4880ab3f4493cccdad12b8d6d7d18e518373d6a3
JIRafid <jubayerishfaqrafid@gmail.com>
2025-08-14 13:31:06 +0600

    Fixed Base Color

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button"
 
 function App() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
+    <div className="flex flex-col items-center justify-center min-h-svh">
       <Button>Click me</Button>
     </div>
   )
